8ba28f8ca6b9395e5edaaa3a02fca8f7ec88e4c1
ViniBiss02 <viniciusbiss02@gmail.com>
2026-07-02 17:28:29 -0300

    Refactor data entry functions and enhance CSV handling for improved user input

--- data_entry.py
+++ data_entry.py
@@ -1,24 +1,37 @@
 from datetime import datetime
 
-date_format = "%Y-%m-%d"
+date_format = "%d-%m-%y"
+CATEGORIES = {"I": "Income", "E": "Expense"}
 
 def get_date(prompt, allow_default=False):
     date_str = input(prompt)
     if allow_default and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(date_format)
 
     try:
-        valid_date = datetime.strptime(date_str, "%d-%m-%Y")
-        return valid_date.strftime("%d-%m-%Y")
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
     except ValueError:
-        print("Invalid date format, please enter a valid date in 'day-mount-year' format'")
+        print("Invalid date format, please enter a valid date in 'day-month-year' format'")
         return get_date(prompt, allow_default)
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be greater than 0")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
 
 def get_category():
-    pass
+    category = input("Enter the category ('I' for Income and 'E' for Expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+
+    print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
+    return get_category()
 
 def get_description():
-    pass
+    return input("Enter the description: ")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_description, get_date
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -27,5 +28,15 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully.")
 
-CSV.initialize_csv()
-CSV.add_entry("20-06-2026", 20.00, "Outcome", "Lunch")
+def add():
+    CSV.initialize_csv()
+    date = get_date(
+        "Enter the date of the transaction (dd/mm/yyyy): ",
+        allow_default=True)
+
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+add()
